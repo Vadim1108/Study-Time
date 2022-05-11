a11dcc4414d355ae1cab6adbbe63b03dd1d44fdf
Vadym_K <kononyhin.vadim1@gmail.com>
2022-05-11 14:19:38 +0300

    Create 1 field and wrote methods to work with SQL database

--- Study Time Tracker/MainWindow.xaml.cs
+++ Study Time Tracker/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SQLite;
+using System.Windows.Forms;
 
 namespace Study_Time_Tracker
 {
@@ -25,9 +27,55 @@ namespace Study_Time_Tracker
             InitializeComponent();
         }
 
-        private void ResetButton1_Click(object sender, RoutedEventArgs e)
+        public SQLiteConnection connection;
+        string SQLPath = "Data Source=Data.db";
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            TextBox1.Text = "Hello";
+            try
+            {
+                connection = new SQLiteConnection(SQLPath);
+                connection.Open();
+
+                TextField1.Text = SQLReadValue(1, "Fields");
+                TextField1Hours.Text = SQLReadValue(1, "Hours");
+                TextField1Minutes.Text = SQLReadValue(1, "Minutes");
+            }
+            catch
+            {
+                System.Windows.Forms.MessageBox.Show("Something goes wrong during loading", "Error", MessageBoxButtons.OK);
+            }
+        }
+
+        string SQLReadValue(int ID, string columnName)
+        {   
+            SQLiteCommand command = new SQLiteCommand
+            { Connection = connection, CommandText = $"SELECT {columnName} FROM ApplicationData WHERE ID={ID}" };
+            
+            SQLiteDataReader SQLReader = command.ExecuteReader();
+
+            while (SQLReader.Read())
+                return $@"{SQLReader[$"{columnName}"]}";
+            return "";
+        } 
+
+        void SQLUpdateField (int ID, string newFieldText)
+        {
+            SQLiteCommand command = new SQLiteCommand
+            { Connection = connection, CommandText =$"UPDATE ApplicationData set Fields='{newFieldText}' WHERE ID={ID}" };
+            command.ExecuteNonQuery();
+
+        }
+
+        private void TextField1_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                SQLUpdateField(1, TextField1.Text);
+                TextField1.Text = SQLReadValue(1, "Fields");
+
+            }
         }
     }
 }
+
